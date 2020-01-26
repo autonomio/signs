845f65df123f066d0f8d773fb5065f9bcfd628db
C0NTRIBUTE <c0L0RC0D3R1976>
2020-01-26 16:44:29 +0200

    Improve SpaCy default behavior for getting the vectors

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ MAINTAINER_EMAIL = 'mailme@mikkokotila.com'
 URL = 'http://autonom.io'
 LICENSE = 'MIT'
 DOWNLOAD_URL = 'https://github.com/autonomio/signs/'
-VERSION = '0.2.4'
+VERSION = '0.3.0'
 
 try:
     from setuptools import setup
