701ad39786eae59f3afc55aa3c24ddd3589e07cc
Mikko Kotila <mailme@mikkokotila.com>
2020-07-22 15:36:10 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ MAINTAINER_EMAIL = 'mailme@mikkokotila.com'
 URL = 'http://autonom.io'
 LICENSE = 'MIT'
 DOWNLOAD_URL = 'https://github.com/autonomio/signs/'
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 
 try:
     from setuptools import setup
