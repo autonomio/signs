df99e8f416c65a281556d18b41f6d6d9c192d478
Mikko Kotila <upstairs@Mikkos-Mac-mini.local>
2022-01-29 18:23:19 +0200

    update CI

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ MAINTAINER_EMAIL = 'mailme@mikkokotila.com'
 URL = 'http://autonom.io'
 LICENSE = 'MIT'
 DOWNLOAD_URL = 'https://github.com/autonomio/signs/'
-VERSION = '0.3.1'
+VERSION = '0.3.2'
 
 try:
     from setuptools import setup
