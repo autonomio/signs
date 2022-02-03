39e4f62d6f74634f1d8d997c6e8eb818902ef083
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2022-02-04 01:36:57 +0530

    generate huggingface embeddings

---
+++ signs/vectorize/huggingface_embeddings.py
@@ -0,0 +1,21 @@
+from transformers import AutoModel,AutoTokenizer
+
+class HuggingfaceEmbeddings:
+    """
+    loads a `model` and `tokenizer` from huggingface from the given 
+        checkpoint if in local storage or huggingface hub
+    """
+    def __init__(self,checkpoint = "prajjwal1/bert-small"):
+        self.checkpoint=checkpoint
+        self.tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
+        self.model = AutoModel.from_pretrained(self.checkpoint)
+    def huggingface_generate_embeddings(self,docs):
+        """
+        Generate huggingface embeddings.
+        Input is a `string` or a `list` of `strings` to be encoded. 
+        """
+        model=self.model
+        tokenizer=self.tokenizer
+        inputs = tokenizer(docs, padding=True, truncation=True, return_tensors="pt")
+        outputs = model(**inputs)
+        return outputs.last_hidden_state
